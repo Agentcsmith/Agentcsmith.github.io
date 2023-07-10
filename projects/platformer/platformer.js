e478ccd3f9c8e75a26ae3edf3c22a2f4174efee8
Christopher Smith <chrisconumopspark@gmail.com>
2023-07-10 19:24:35 +0000

    working on project

--- projects/platformer/platformer.js
+++ projects/platformer/platformer.js
@@ -29,14 +29,14 @@ $(function () {
      * Comment the lines out to remove the grid
      */
 
-    // Loop to create vertical grid lines
-    // for (let i = 100; i < canvas.width; i += 100) {
-    //   createPlatform(i, 0, 1, canvas.height);
-    // }
+     //Loop to create vertical grid lines
+    // //for (let i = 100; i < canvas.width; i += 100) {
+    //    createPlatform(i, 0, 1, canvas.height);
+    //  }
 
     // Loop to create horizontal gride lines
     // for (let i = 100; i < canvas.height; i += 100) {
-    //   createPlatform(0, i, canvas.width, 1);
+    //    createPlatform(0, i, canvas.width, 1);
     // }
 
     /////////////////////////////////////////////////
@@ -47,6 +47,9 @@ $(function () {
     // Create platforms
     // You must decide the x position, y position, width, and height of the platforms
     // example usage: createPlatform(x,y,width,height)
+    createPlatform(200, 50, 100, 50); //top
+   
+
 
 
 
